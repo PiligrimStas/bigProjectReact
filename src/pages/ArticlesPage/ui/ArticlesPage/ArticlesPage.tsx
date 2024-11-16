3286ea6296cf8d95634ad0ffc8adf917f3d1cd53
Stanislav <kislorod1336@protonmail.com>
2024-11-16 16:11:08 +0300

    added-articles-pages

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,22 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './ArticlesPage.module.scss';
+
+interface ArticlesPageProps {
+    className?: string;
+}
+
+const ArticlesPage = (props: ArticlesPageProps) => {
+    const { className } = props;
+    const { t } = useTranslation('article');
+
+    return (
+        <div className={classNames(cls.ArticlePage, {}, [className])}>
+            {t('This is the page of articles')}
+        </div>
+    );
+};
+
+// lazy работает только с импортами по умаолчанию
+export default memo(ArticlesPage);
